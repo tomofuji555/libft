6bfa811177893037efca25e46b8ef840b0ee4a8e
tomofuji555 <tomokifujiwara321@outlook.jp>
2023-02-14 15:17:46 +0900

    trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tofujiwa <tofujiwa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 18:43:10 by tofujiwa          #+#    #+#             */
-/*   Updated: 2023/02/14 15:03:01 by tofujiwa         ###   ########.fr       */
+/*   Updated: 2023/02/14 15:16:32 by tofujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	end = ft_strlen(s1);
 	while (ft_strchr((char *)set, s1[start]) && s1)
 		start++;
-	while (ft_strrchr((char *)set, s1[end]) && s1 && end > start)
+	while (ft_strrchr((char *)set, s1[end]) && s1 && end >= start)
 		end--;
 	if (start > end)
-		return (NULL);
+		return (ft_strdup(""));
 	trim = ft_substr(s1, start, end - start + 1);
 	return (trim);
 }
