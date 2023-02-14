01e711497653449aa691f483ced1efddede77845
tomofuji555 <tomokifujiwara321@outlook.jp>
2023-02-14 15:29:59 +0900

    ft_trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tofujiwa <tofujiwa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 18:43:10 by tofujiwa          #+#    #+#             */
-/*   Updated: 2023/02/14 15:16:32 by tofujiwa         ###   ########.fr       */
+/*   Updated: 2023/02/14 15:29:15 by tofujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
 	size_t	start;
 	size_t	end;
 
@@ -28,6 +27,5 @@ char	*ft_strtrim(char const *s1, char const *set)
 		end--;
 	if (start > end)
 		return (ft_strdup(""));
-	trim = ft_substr(s1, start, end - start + 1);
-	return (trim);
+	return (ft_substr(s1, start, end - start + 1));
 }
